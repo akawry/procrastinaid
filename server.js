09c4dc31b1c7405faef27d11c21812d2fcbb6a02
Alex Kawrykow <akawry@cs.mcgill.ca>
2012-04-21 18:58:58 -0400

    adding mongodb

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express.createServer();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/procrastinaid');
 
 app.get('/', function(req, res){
 	res.end("Hello world!");
