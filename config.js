80f138d10d431c8635447b29db0566c9fe63ae67
akawry <akawry@cs.mcgill.ca>
2012-05-15 23:02:14 -0300

    Editing out sensitive data

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 exports.fb = {
-	app_id : "349863548395860",
-	app_secret: "1e081f4afe049d72ab5ab2a75cfd7a35"
+	app_id : "XXXXXXXXXXXXXXX",
+	app_secret: "XXXXXXXXXXXXXXX"
 };
 
 exports.amazon = {
-	id: 'AKIAICGDJXAFYZ23LHTA', 
-	secret: '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'
+	id: 'XXXXXXXXXXXXXXX', 
+	secret: 'XXXXXXXXXXXXXXX'
 };
 
 exports.express = {
-	session_secret: 'keyboard cat'
+	session_secret: 'XXXXXXXXXXXXXXX'
 };
 
 exports.email = {
@@ -17,4 +17,4 @@ exports.email = {
 	replyTo: ['no-reply@procrastinaid.in']
 };
 
-exports.site_url = "http://ec2-50-17-70-185.compute-1.amazonaws.com";
+exports.site_url = "http://XXXXXXXXXXXXXXX.compute-1.amazonaws.com";
