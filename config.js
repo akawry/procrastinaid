c6a4cc315802bbb2e1387f080427fc8a0dfde28e
Alex Kawrykow <akawry@cs.mcgill.ca>
2012-05-16 20:00:19 -0400

    resetting configs

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 exports.fb = {
-	app_id : "XXXXXXXXXXXXXXX",
-	app_secret: "XXXXXXXXXXXXXXX"
+	app_id : "349863548395860",
+	app_secret: "1e081f4afe049d72ab5ab2a75cfd7a35"
 };
 
 exports.amazon = {
-	id: 'XXXXXXXXXXXXXXX', 
-	secret: 'XXXXXXXXXXXXXXX'
+	id: 'AKIAICGDJXAFYZ23LHTA', 
+	secret: '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'
 };
 
 exports.express = {
-	session_secret: 'XXXXXXXXXXXXXXX'
+	session_secret: 'keyboard cat'
 };
 
 exports.email = {
@@ -17,4 +17,4 @@ exports.email = {
 	replyTo: ['no-reply@procrastinaid.in']
 };
 
-exports.site_url = "http://XXXXXXXXXXXXXXX.compute-1.amazonaws.com";
+exports.site_url = "http://ec2-50-17-70-185.compute-1.amazonaws.com";
